657d8cc300d5974bb17d1c960ed6a12a96bd4a7e
Dayvson Silva <userddssilva@gmail.com>
2022-10-22 22:05:41 -0400

    chore: reorganize install omz

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@
 ## commands to install softwares 
 source installs/install.sh 
 
+## install Oh My Zshell
+source install/install_oh_my_zsh.sh
+
 ## commands to machine config 
-source settings/configurations.sh
+source settings/config.sh
