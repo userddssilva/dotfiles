c7378a90fc44de0ee41264d7092885951af9d5f9
Dayvson Silva <userddssilva@gmail.com>
2022-10-24 10:02:01 -0400

    update: join install and settings to install oh_my_zsh

--- installs/install_oh_my_zsh.sh
+++ installs/install_oh_my_zsh.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# add commands extras to ~/.zshrc
+cat ../settings/.aliases >> ~/.zshrc
+
+# set oh my zsh theme
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="fino-time"/g' ~/.zshrc
+
+# reload ~/.zshrc settings
+source ~/.zshrc
--- settings/config_oh_my_zsh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# add commands extras to ~/.zshrc
-cat .aliases >> ~/.zshrc
-
-# set oh my zsh theme
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="fino-time"/g' ~/.zshrc
